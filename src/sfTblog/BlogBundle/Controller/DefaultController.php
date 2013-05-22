0bccdba131f25cc92edb04240e42bb308378afb7
dkhaleev <dkhaleev@gmail.com>
2013-05-22 23:14:03 +0300

    generated blog bundle and user bundle

---
+++ src/sfTblog/BlogBundle/Controller/DefaultController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace sfTblog\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class DefaultController extends Controller
+{
+    
+    public function indexAction($name)
+    {
+        return $this->render('sfTblogBlogBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/sfTblog/BlogBundle/sfTblogBlogBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace sfTblog\BlogBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class sfTblogBlogBundle extends Bundle
+{
+}
