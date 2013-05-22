09a051e714247b2f4cb217dacee038b316d99b3f
dkhaleev <dkhaleev@gmail.com>
2013-05-22 23:59:43 +0300

    dummy index action

--- src/sfTblog/BlogBundle/Controller/DefaultController.php
+++ src/sfTblog/BlogBundle/Controller/DefaultController.php
@@ -8,8 +8,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     
-    public function indexAction($name)
+    public function indexAction($name=false)
     {
-        return $this->render('sfTblogBlogBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('sfTblogBlogBundle:Partials:index.html.twig', array('name' => $name));
     }
 }
