4675a6be4b554a31ae058788be8af85c0f36cc18
dkhaleev <dkhaleev@gmail.com>
2013-07-03 22:04:34 +0300

    Independent templates blanks for each action of the homepage controller

--- src/sfTblog/BlogBundle/Controller/HomepageController.php
+++ src/sfTblog/BlogBundle/Controller/HomepageController.php
@@ -17,63 +17,63 @@ class HomepageController extends Controller
     public function lineAction()
     {
         return $this
-                ->render('sfTblogBlogBundle:Homepage:context.html.twig',
+                ->render('sfTblogBlogBundle:Homepage:line-ad.html.twig',
                         array('title' => "строчная реклама"));
     }
 
     public function auditAction()
     {
         return $this
-                ->render('sfTblogBlogBundle:Homepage:context.html.twig',
+                ->render('sfTblogBlogBundle:Homepage:audit.html.twig',
                         array('title' => "аудит аналитика сайта"));
     }
 
     public function contentAction()
     {
         return $this
-                ->render('sfTblogBlogBundle:Homepage:context.html.twig',
+                ->render('sfTblogBlogBundle:Homepage:content.html.twig',
                         array('title' => "контент"));
     }
 
     public function actPromoAction()
     {
         return $this
-                ->render('sfTblogBlogBundle:Homepage:context.html.twig',
+                ->render('sfTblogBlogBundle:Homepage:promo.html.twig',
                         array('title' => "активное продвижение"));
     }
 
     public function vkPromoAction()
     {
         return $this
-                ->render('sfTblogBlogBundle:Homepage:context.html.twig',
+                ->render('sfTblogBlogBundle:Homepage:vk_promo.html.twig',
                         array('title' => "продвижение ВКонтакте"));
     }
 
     public function reputationAction()
     {
         return $this
-                ->render('sfTblogBlogBundle:Homepage:context.html.twig',
+                ->render('sfTblogBlogBundle:Homepage:reputation.html.twig',
                         array('title' => "Управление репутацией компании"));
     }
 
     public function bannersAction()
     {
         return $this
-                ->render('sfTblogBlogBundle:Homepage:context.html.twig',
+                ->render('sfTblogBlogBundle:Homepage:banners.html.twig',
                         array('title' => "Изготовление баннеров"));
     }
 
     public function consultationAction()
     {
         return $this
-                ->render('sfTblogBlogBundle:Homepage:context.html.twig',
+                ->render('sfTblogBlogBundle:Homepage:consultation.html.twig',
                         array('title' => "услуги консультации"));
     }
     
     public function requestAction()
     {
         return $this
-        ->render('sfTblogBlogBundle:Homepage:context.html.twig',
+        ->render('sfTblogBlogBundle:Homepage:request.html.twig',
                 array('title' => "подать заявку"));
     }
 }
